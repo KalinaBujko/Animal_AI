6a58ddb7debffae05349b6d200f19c69aaa1506e
KalinaBujko <kiki.b2002@gmail.com>
2026-01-01 23:57:22 +0100

    Plugin update

---
+++ Plugins/AIController/Source/AIController/Private/AIControllerBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AIControllerBPLibrary.h"
+#include "AIController.h"
+
+UAIControllerBPLibrary::UAIControllerBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UAIControllerBPLibrary::AIControllerSampleFunction(float Param)
+{
+	return -1;
+}
+
